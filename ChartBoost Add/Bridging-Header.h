c0207d3d0fce3842bd60028e2110061eb0b2d355
Dharmesh <khenid21@gmail.com>
2015-02-23 12:04:08 +0530

    ChartBoost With Swift
    ChartBoost With Swift

---
+++ ChartBoost Add/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  ChartBoost Add
+//
+//  Created by Anil on 23/02/15.
+//  Copyright (c) 2015 Variya Soft Solutions. All rights reserved.
+//
+
+
+#import <Chartboost/Chartboost.h>
+#import <Chartboost/CBNewsfeed.h>
+#import <CommonCrypto/CommonDigest.h>
+#import <AdSupport/AdSupport.h>
